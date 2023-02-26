6fe764938c9b3e32fb6b5498ecc034b189b3299c
S. M Shawal Chowdhury <shafayet.shawal@gmail.com>
2023-02-26 17:34:27 +0600

    remove console.log

--- models/userModel.js
+++ models/userModel.js
@@ -86,7 +86,6 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
       this.passwordChangedAt.getTime() / 1000,
       10
     );
-    console.log(JWTTimestamp, changedTimestamp);
     return JWTTimestamp < changedTimestamp;
   }
 
@@ -101,8 +100,6 @@ userSchema.methods.createPasswordResetToken = function () {
     .createHash("sha256")
     .update(resetToken)
     .digest("hex");
-
-  console.log({ resetToken }, this.passwordResetToken);
 
   this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
 
